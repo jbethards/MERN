a377c075f845dc9868a32ebc6637d99ac020cad9
jbethards <jbethards2984@smail.pcd.edu>
2018-07-09 15:59:16 -0700

    Ability to post user information through postman

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -33,6 +33,58 @@ router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
     })
     .catch(err=>res.status(404).json(err));
 });
+//@route GET api/profile/all
+//@desc Get all profiles
+//@access public
+router.get('/all',(req,res)=>{
+    Profile.find()
+    .populate('user',['name','avatar'])
+    .then(profiles=>{
+        if(!profiles){
+            errors.noprofile="There are profile for this user";
+            return res.status(404).json(errors);
+        }
+        res.json(profiles)
+    }).catch(err=>
+        res.status(404).json({profile:'There are no profiles'})
+    );
+});
+
+
+
+//@route GET api/profile/handle/handle
+//@desc Get Profile by handle
+//@access public
+router.get('/handle/:handle',(req,res)=>{
+    const errors={};
+    Profile.findOne({handle:req.params.handle})
+    .populate('user',['name','avatar'])
+    .then(profile=>{
+        if(!profile){
+            error.noprofile="There is no profile for this user.";
+            res.status(404),json(errors);
+        }
+        res.json(profile);
+    }).catch(err=>res.status(404).json(err));
+});
+//@route GET api/profile/user/:user_id
+//@desc Get Profile by user_id
+//@access public
+router.get('/user/:user_id',(req,res)=>{
+    const errors={};
+    Profile.findOne({user:req.params.user_id})
+    .populate('user',['name','avatar'])
+    .then(profile=>{
+        if(!profile){
+            error.noprofile="There is no profile for this user.";
+            res.status(404),json(errors);
+        }
+        res.json(profile);
+    }).catch(err=>res.status(404).json({profile:"There is no prfile for this user."}));
+});
+
+
+
 //@route POST api/profile
 //@desc create or edit current users profile
 //@access Private
