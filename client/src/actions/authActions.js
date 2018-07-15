bbfdfde3a156af41f21b8714040e79a3f6e0878b
jbethards <jbethards2984@smail.pcd.edu>
2018-07-14 22:05:29 -0700

    Added auth files

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,7 @@
+import {TEST_DISPATCH} from './Types';
+export const registerUser =userData=>{
+    return{
+        type: TEST_DISPATCH,
+        payload:userData
+    };
+}
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import axios from 'axios';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
  class Register extends Component {
     constructor(){
@@ -28,14 +31,17 @@ onSubmit(e){
         password:this.state.password,
         password2:this.state.password2
     }
-    axios.post('/api/users/register',newUser)
-    .then(res=>console.log(res.data))
-    .catch(err=>this.setState({errors:err.response.data}));
+    this.props.registerUser(newUser);
+    //axios.post('/api/users/register',newUser)
+    //.then(res=>console.log(res.data))
+    //.catch(err=>this.setState({errors:err.response.data}));
 }
   render() {
     const{errors}=this.state;
+    const{user}=this.props.auth;
     return (
         <div className="register">
+        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -101,4 +107,11 @@ onSubmit(e){
     )
   }
 }
-export default Register;
+Register.PropTypes={
+  registerUser:PropTypes.func.isRequired,
+  auth:PropTypes.object.isRequired
+}
+const mapStateToProps =(state)=>({
+  auth : state.auth
+});
+export default connect(mapStateToProps,{registerUser})(Register);
